361756f5f6dd6c1bc843be45975b5bb4322b5e15
Acsbeni <acsbenjamin@turr.hu>
2026-03-05 13:51:23 +0100

    SER-48 login oldal elkezdése, bootstrap behelyezése

---
+++ Frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log('Login attempt:', this.email, this.password);
+    
+  }
+}
